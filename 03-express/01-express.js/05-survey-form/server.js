467c09090056cb65344130bfdbea2df108b9593c
dawit <dawittk2004@yahoo.com>
2019-03-02 20:58:23 -0500

    first commit

---
+++ 03-express/01-express.js/05-survey-form/server.js
@@ -0,0 +1,48 @@
+// require express
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+// create the express app
+var app = express();
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+ res.render("index");
+})
+app.post('/result', function(req, res) {
+    console.log("POST DATA", req.body);
+    submitted_Info = {
+        name : req.body.name,
+        location : req.body.location,
+        language : req.body.language,
+        description : req.body.description
+    };
+    res.render('result',{data : submitted_Info})
+
+    // This is where we would add the user to the database
+    // Then redirect to the root route
+   
+   })
+// post route for adding a user
+app.post('/goback', function(req, res) {
+       // This is where we would add the user to the database
+    // Then redirect to the root route
+    res.redirect('/');
+   })
+app.post('/users', function(req, res) {
+ console.log("POST DATA", req.body);
+ // This is where we would add the user to the database
+ // Then redirect to the root route
+ res.redirect('/');
+})
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
